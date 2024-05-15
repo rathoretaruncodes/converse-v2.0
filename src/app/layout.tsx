8c8e952fd6b5a5980782ce7959058c81b2e6a396
rathoretaruncodes <rathoretaruncodes@gmail.com>
2024-05-15 20:39:21 +0530

    ui:Update application font for improved readability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Orbitron } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const font = Orbitron({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={font.className}>
         <ThemeProvider
             attribute="class"
             defaultTheme="dark"
