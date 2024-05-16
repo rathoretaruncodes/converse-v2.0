2bde7259d5bd8ec9176143a3e9fc6be574f6ff14
rathoretaruncodes <rathoretaruncodes@gmail.com>
2024-05-16 09:01:17 +0530

    ui:design initial layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
-export default function Home() {
+export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="max-w-3xl h-screen mx-auto pd:py-10">
       CONVERSE 2.0
-    </main>
+      <div className="h-full border rounded-md">
+        <div className="h-20">
+          <div className="flex items-center justify-between p-5 border-b">
+            <div>
+              <div className="text-xl font-bold">Daily Chat</div>
+              <div className="flex items-center gap-1">
+                <div className="h-4 w-4 bg-green-500 rounded-full animate-pulse" />
+                <div className="text-sm text-gray-400">2 people online</div>
+              </div>
+            </div>
+            <Button>Login</Button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
